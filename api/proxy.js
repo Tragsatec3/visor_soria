609f4cba9a69c13785d77a839d5109d9558df7c5
Tragsatec3 <flastres@tragsa.es>
2024-11-05 20:46:54 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,20 +1,19 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// Configuramos el middleware en Node.js
-const proxy = createProxyMiddleware({
-    target: 'http://mi-servidor-wms.com', // Cambia esta URL a tu servidor WMS
-    changeOrigin: true,
-    pathRewrite: {
-        '^/api/proxy': '', // Elimina el prefijo /api/proxy en la redirección
-    },
-    onProxyRes(proxyRes) {
-        proxyRes.headers['Access-Control-Allow-Origin'] = '*'; // Permite CORS
-    },
-});
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export default function handler(req, res) {
-    // Ejecutamos el middleware
-    return proxy(req, res, () => {
-        res.status(500).send("Error en el proxy");
-    });
+  const proxy = createProxyMiddleware({
+    target: 'http://ovc.catastro.meh.es/Cartografia/WMS/ServidorWMS.aspx',
+    changeOrigin: true,
+    pathRewrite: { '^/api/proxy': '' }, // Elimina '/api/proxy' del inicio de la ruta
+    onProxyRes(proxyRes) {
+      proxyRes.headers['Access-Control-Allow-Origin'] = '*'; // Permite CORS
+    },
+  });
+
+  proxy(req, res, (err) => {
+    if (err) {
+      console.error('Error en el proxy:', err);
+      res.status(500).send("Error en el proxy: " + err.message);
+    }
+  });
 }
